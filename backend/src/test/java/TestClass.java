8706d4e6e0746f6f67b133eeee89f4edacb25a3a
Joshua Capononog <joshua.caponong@nokia.com>
2019-11-29 13:46:49 +0800

    update gitignore

---
+++ backend/src/test/java/TestClass.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class TestClass {
+    @Test
+    public void wew() {
+        Set<String> myCodes = new HashSet<>();
+//        System.out.println(generateUniqueGameCode(myCodes));
+    }
+
+
+
+}
